7732e6c6f318d3696c5b5404904ec09513ed8058
Pedro Kalva Junior <pedro.kalva@gmail.com>
2023-09-17 22:43:08 -0300

    creating home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
     selector: "app-home-page",
     templateUrl: "./home.component.html",
-    imports: [ ],
+    imports: [ MatFormFieldModule, MatButtonModule, MatInputModule, MatIconModule, MatButtonToggleModule ],
     standalone: true,
 })
 
