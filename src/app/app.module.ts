ab68bccd08fb8019e5cad1025e5d56284ca9eb74
Pedro Kalva Junior <pedro.kalva@gmail.com>
2023-09-18 19:48:11 -0300

    Validate form searching for match

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -15,7 +17,7 @@ import { HeaderComponent } from './layout/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
-import { FormControl,  FormGroup,  ReactiveFormsModule,  Validators,} from "@angular/forms";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, FormBuilder} from "@angular/forms";
+import { NgIf } from "@angular/common";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input'
@@ -15,28 +16,46 @@ interface SearchMatch {
 @Component({
     selector: "app-home-page",
     templateUrl: "./home.component.html",
-    imports: [ MatFormFieldModule, MatButtonModule, MatInputModule, MatIconModule, MatButtonToggleModule, ReactiveFormsModule ],
+    imports: [ MatFormFieldModule, MatButtonModule, MatInputModule, MatIconModule
+      , MatButtonToggleModule, ReactiveFormsModule, NgIf ],
     standalone: true,
 })
 
 export class HomeComponent {
-[x: string]: any;
-
-    isSubmitting = false
+    isSubmitting = false;
+    backgroundUrl = '';
+    summoner = new FormControl('', [Validators.required]);
+    region = new FormControl('', [Validators.required]);
 
     searchForm = new FormGroup<SearchMatch>({
-      summoner: new FormControl("", { nonNullable: true}),
-      region: new FormControl("", { nonNullable: true})
+      summoner: new FormControl('', { nonNullable: true}),
+      region: new FormControl('br', { nonNullable: true})
     })
 
-    constructor(private readonly router: Router) { }
+    constructor( private readonly router: Router) { }
+
+    ngOnInit(): void {
+      
+    }
+
+    getErrorSummoner() {
+      if (this.summoner.hasError('required'))
+        return 'You must enter a summoner name';
+  
+      return '';
+    }
 
     onSubmit() 
     { 
-      this.isSubmitting = true;
       const summoner = this.searchForm.get('summoner')?.value;
       const region = this.searchForm.get('region')?.value;
 
+      if (!summoner)
+        return alert("You must enter a summoner name");
+
+      this.isSubmitting = true;
+
       // void this.router.navigate(["/match/"/*, param */])
+      this.isSubmitting = false;
     }
 }
