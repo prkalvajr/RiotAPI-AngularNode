864216ee5604803ee7453d96845889bdf61b8971
Pedro Kalva Junior <pedro.kalva@gmail.com>
2023-10-10 23:39:38 -0300

    cleaning code

--- src/app/components/match/match.component.ts
+++ src/app/components/match/match.component.ts
@@ -54,65 +54,41 @@ export class MatchComponent implements OnInit, OnDestroy {
           });
         })
       )
- 
-
-
-        //zip(this.service.fetchRankData)
-        //zip(participants => { 
-        //  return zip(participants.map((i: { summonerId: string; }) => 
-        //  this.service.fetchRankData(i.summonerId, this.region))
-        //)})        
-        
-        //TESTAR
-      
-
       .subscribe((data: any) => {
-
         const observer = {
-          next: (rank: any) => {
+          next: (data: any) => {
             debugger;
-              const rIndex = rank.findIndex((x: any) => x.queueType == "RANKED_SOLO_5x5")
-              const index = this.cardsData.findIndex((x) => x.summonerId == rank[rIndex].summonerId)
 
-              let tierHasNoRank = false;
-              if (rank[0].tier == 'MASTER' || rank[0].tier == 'GRANDMASTER' || rank[0].tier == 'CHALLENGER')
-              tierHasNoRank = true;
-              
-              this.cardsData[index].tier = rank[rIndex].tier;
-              this.cardsData[index].rank = tierHasNoRank ? '' : rank[rIndex].rank;
-              this.cardsData[index].leaguePoints = rank[rIndex].leaguePoints;
+            const rIndex = data[1].findIndex((x: any) => x.queueType == "RANKED_SOLO_5x5")
+
+            let tierHasNoRank = false;
+            if (data[1][rIndex].tier == 'MASTER' || data[1][rIndex].tier == 'GRANDMASTER' || data[1][rIndex].tier == 'CHALLENGER')
+            tierHasNoRank = true;
+
+            this.cardsData.push({ championId: data[0].championId, 
+              championName: 'a',//champName,
+              summonerName: data[0].summonerName,
+              summonerId: data[0].summonerId,
+              icon: '', //iconurl,
+              spell1Id: '', //spell1Url,
+              spell2Id: '', //spell2Url,
+              tier: data[1][rIndex].tier,
+              rank: tierHasNoRank ? '' : data[1][rIndex].rank,
+              leaguePoints: data[1][rIndex].leaguePoints 
+            });
           }
         }
 
         data.subscribe(observer);
 
-        //data.map((participant: { summonerId: string; championId: string; summonerName: string;
-          //spell1Id: string; spell2Id: string; }) => {
+          //const championJson = this.service.getDataDragonChampion();
 
-          const championJson = this.service.getDataDragonChampion();
           //const champName = this.findKeyInJson(championJson.data, participant.championId);
           //const iconurl = constants.DDRAGON_CHAMPION_ICON_ROUTE + champName + ".png";
           //const spell1Name = this.findKeyInJson(summonerJson.data, participant.spell1Id);
           //const spell1Url = constants.DDRAGON_CHAMPION_SUMMONERSPELL_ROUTE + spell1Name + ".png";
           //const spell2Name = this.findKeyInJson(summonerJson.data, participant.spell2Id);
           //const spell2Url = constants.DDRAGON_CHAMPION_SUMMONERSPELL_ROUTE + spell2Name + ".png";
-
-          /*
-          this.cardsData.push({ championId: participant.championId, 
-            championName: 'a',//champName,
-            summonerName: participant.summonerName,
-            summonerId: participant.summonerId,
-            icon: '', //iconurl,
-            spell1Id: '', //spell1Url,
-            spell2Id: '', //spell2Url,
-            tier: '', //rankJson[0].tier,
-            rank: '', //tierHasNoRank ? '' : rankJson[0].rank,
-            leaguePoints: '' //rankJson[0].leaguePoints 
-          });
-
-          this.service.fetchRankData(participant.summonerId, this.region).subscribe(rankObserver);
-        });
-        */
       }); 
       
       this.subscriptions.push(sub);
